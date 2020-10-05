22f46cb624bcddbafa3f66d27bf0339a8ad7283f
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-06 00:36:43 +0530

    [Shubham]Refactored: User validation using lambda

--- src/main/java/com/cagpi/userregistrationg/UserRegistrationGradle.java
+++ src/main/java/com/cagpi/userregistrationg/UserRegistrationGradle.java
@@ -9,8 +9,8 @@ interface iValidation {
 }
 
 public class UserRegistrationGradle {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static boolean Validation(String fName, String lName, String mail, String phone, String passWord)
+			throws UserRegistrationCustomException {
 		Pattern patternFirstName = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
 		Pattern patternLastName = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
 		Pattern patternEmail = Pattern
@@ -19,78 +19,107 @@ public class UserRegistrationGradle {
 		Pattern patternPassword = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{8,}$");
 
 		iValidation firstName = (name) -> {
-			Matcher matcher = patternFirstName.matcher(name);
+			Matcher matcher = patternFirstName.matcher(fName);
 			boolean isValid = matcher.find();
 			if (isValid)
 				return true;
+
 			else
 				return false;
 		};
+
 		iValidation lastName = (name) -> {
-			Matcher matcher = patternLastName.matcher(name);
+			Matcher matcher = patternLastName.matcher(lName);
 			boolean isValid = matcher.find();
 			if (isValid)
 				return true;
 			else
 				return false;
 		};
+
 		iValidation email = (name) -> {
-			Matcher matcher = patternEmail.matcher(name);
+			Matcher matcher = patternEmail.matcher(mail);
 			boolean isValid = matcher.find();
 			if (isValid)
 				return true;
 			else
 				return false;
+
 		};
 		iValidation phoneNo = (name) -> {
-			Matcher matcher = patternPhoneNo.matcher(name);
+			Matcher matcher = patternPhoneNo.matcher(phone);
 			boolean isValid = matcher.find();
 			if (isValid)
 				return true;
 			else
 				return false;
+
 		};
 		iValidation password = (name) -> {
-			Matcher matcher = patternPassword.matcher(name);
+			Matcher matcher = patternPassword.matcher(passWord);
 			boolean isValid = matcher.find();
 			if (isValid)
 				return true;
 			else
 				return false;
 		};
+		boolean isValid = firstName.validation(fName);
+		if (isValid)
+			System.out.println("Valid first name");
+		else
+			throw new UserRegistrationCustomException("Enter Valid first name");
+		isValid = lastName.validation(lName);
+		if (isValid)
+			System.out.println("valid last name");
+		else
+			throw new UserRegistrationCustomException("Enter Valid last name");
+		isValid = email.validation(mail);
+		if (isValid)
+			System.out.println("Valid email");
+		else
+			throw new UserRegistrationCustomException("Enter Valid email");
+		isValid = phoneNo.validation(phone);
+		if (isValid)
+			System.out.println("Valid phone number");
+		else
+			throw new UserRegistrationCustomException("Enter Valid phone number");
+		isValid = password.validation(passWord);
+		if (isValid)
+			System.out.println("Valid password");
+		else
+			throw new UserRegistrationCustomException("Enter Valid password");
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
 		boolean check = false;
 		while (!check) {
 			System.out.println("Enter First Name");
 			String fName = sc.nextLine();
-			if (firstName.validation(fName)) {
-				System.out.println("Valid Firstname");
-			} else
-				System.out.println("Invalid Firstname");
+
 			System.out.println("Enter Last Name");
 			String lName = sc.nextLine();
-			if (lastName.validation(lName)) {
-				System.out.println("Valid Lastname");
-			} else
-				System.out.println("Invalid Lastname");
+
 			System.out.println("Enter Email");
 			String mail = sc.nextLine();
-			if (email.validation(mail)) {
-				System.out.println("Valid Email");
-			} else
-				System.out.println("Invalid Email");
+
 			System.out.println("Enter Phone number");
 			String phone = sc.nextLine();
-			if (phoneNo.validation(phone)) {
-				System.out.println("Valid Phone number");
-			} else
-				System.out.println("Invalid Phone number");
+
 			System.out.println("Enter Password");
 			String passWord = sc.nextLine();
-			if (password.validation(passWord)) {
-				System.out.println("Valid Password");
-				check = true;
-			} else
-				System.out.println("Invalid Password");
+
+			try {
+				check = Validation(fName, lName, mail, phone, passWord);
+
+				System.out.println("Validation Successful");
+
+			} catch (UserRegistrationCustomException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Enter details again");
+			}
 		}
 	}
 }
