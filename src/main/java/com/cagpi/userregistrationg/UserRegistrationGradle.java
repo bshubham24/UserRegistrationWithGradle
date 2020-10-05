f8e5f6f0acaababb2d49b9edf5536edf1dced20c
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-05 23:53:01 +0530

    [Shubham]Refactored: User validation using lambda

--- src/main/java/com/cagpi/userregistrationg/UserRegistrationGradle.java
+++ src/main/java/com/cagpi/userregistrationg/UserRegistrationGradle.java
@@ -4,103 +4,93 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+interface iValidation {
+	public boolean validation(String s);
+}
+
 public class UserRegistrationGradle {
-
-	public boolean ValidateFirstName(String fName) throws UserRegistrationCustomException {
-		Pattern pattern = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
-		Matcher matcher = pattern.matcher(fName);
-		boolean isValid = matcher.find();
-		if (isValid) {
-			System.out.println("Name is valid");
-
-		} else {
-			throw new UserRegistrationCustomException("Name is invalid");
-
-		}
-		return (isValid);
-	}
-
-	public boolean ValidateLastName(String lName) throws UserRegistrationCustomException {
-		Pattern pattern = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
-		Matcher matcher = pattern.matcher(lName);
-		boolean isValid = matcher.find();
-		if (isValid) {
-			System.out.println(" Last Name is valid");
-
-		} else {
-			throw new UserRegistrationCustomException("Last Name is invalid");
-
-		}
-		return (isValid);
-	}
-
-	public boolean ValidateEmail(String email) throws UserRegistrationCustomException {
-		Pattern pattern = Pattern
-				.compile("(^([a-z0-9+_-]+)([.][0-9a-z]+)*@([a-z0-9]+)([.]([a-z]{2,}))([.][a-z]{2})?$)");
-		Matcher matcher = pattern.matcher(email);
-		boolean isValid = matcher.find();
-		if (isValid) {
-			System.out.println("email is valid");
-
-		} else {
-			throw new UserRegistrationCustomException("email is invalid");
-
-		}
-		return (isValid);
-	}
-
-	public boolean ValidatePhoneNo(String phoneNo) throws UserRegistrationCustomException {
-		Pattern pattern = Pattern.compile("(^([0-9]+)( )([1-9]{1})([0-9]{9})$)");
-		Matcher matcher = pattern.matcher(phoneNo);
-		boolean isValid = matcher.find();
-		if (isValid) {
-			System.out.println("phoneNO is valid");
-
-		} else {
-			throw new UserRegistrationCustomException("phoneNo is invalid");
-
-		}
-		return (isValid);
-	}
-
-	public boolean ValidatePassword(String password) throws UserRegistrationCustomException {
-		Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{8,}$");
-		Matcher matcher = pattern.matcher(password);
-		boolean isValid = matcher.find();
-		if (isValid) {
-			System.out.println("password is valid");
-
-		} else {
-			throw new UserRegistrationCustomException("password is invalid");
-
-		}
-		return (isValid);
-	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Pattern patternFirstName = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
+		Pattern patternLastName = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
+		Pattern patternEmail = Pattern
+				.compile("(^([a-z0-9+_-]+)([.][0-9a-z]+)*@([a-z0-9]+)([.]([a-z]{2,}))([.][a-z]{2})?$)");
+		Pattern patternPhoneNo = Pattern.compile("(^([0-9]+)( )([1-9]{1})([0-9]{9})$)");
+		Pattern patternPassword = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{8,}$");
 
-		UserRegistrationGradle userObj = new UserRegistrationGradle();
-
-		/*
-		 * System.out.println("Enter the First name"); String fName = sc.nextLine();
-		 * userObj.ValidateFirstName(fName);
-		 * 
-		 * System.out.println("Enter the last name"); String lName = sc.nextLine();
-		 * userObj.ValidateLastName(lName);
-		 * 
-		 * System.out.println("Enter Email"); String email = sc.nextLine();
-		 * userObj.ValidateEmail(email);
-		 * 
-		 * System.out.println("Enter Phone number"); String phoneNo = sc.nextLine();
-		 * userObj.ValidatePhoneNo(phoneNo);
-		 */
-		System.out.println("Enter Password");
-		String password = sc.nextLine();
-		try {
-			userObj.ValidatePassword(password);
-		} catch (UserRegistrationCustomException e) {
-			System.out.println(e.getMessage());
+		iValidation firstName = (name) -> {
+			Matcher matcher = patternFirstName.matcher(name);
+			boolean isValid = matcher.find();
+			if (isValid)
+				return true;
+			else
+				return false;
+		};
+		iValidation lastName = (name) -> {
+			Matcher matcher = patternLastName.matcher(name);
+			boolean isValid = matcher.find();
+			if (isValid)
+				return true;
+			else
+				return false;
+		};
+		iValidation email = (name) -> {
+			Matcher matcher = patternEmail.matcher(name);
+			boolean isValid = matcher.find();
+			if (isValid)
+				return true;
+			else
+				return false;
+		};
+		iValidation phoneNo = (name) -> {
+			Matcher matcher = patternPhoneNo.matcher(name);
+			boolean isValid = matcher.find();
+			if (isValid)
+				return true;
+			else
+				return false;
+		};
+		iValidation password = (name) -> {
+			Matcher matcher = patternPassword.matcher(name);
+			boolean isValid = matcher.find();
+			if (isValid)
+				return true;
+			else
+				return false;
+		};
+		boolean check = false;
+		while (!check) {
+			System.out.println("Enter First Name");
+			String fName = sc.nextLine();
+			if (firstName.validation(fName)) {
+				System.out.println("Valid Firstname");
+			} else
+				System.out.println("Invalid Firstname");
+			System.out.println("Enter Last Name");
+			String lName = sc.nextLine();
+			if (lastName.validation(lName)) {
+				System.out.println("Valid Lastname");
+			} else
+				System.out.println("Invalid Lastname");
+			System.out.println("Enter Email");
+			String mail = sc.nextLine();
+			if (email.validation(mail)) {
+				System.out.println("Valid Email");
+			} else
+				System.out.println("Invalid Email");
+			System.out.println("Enter Phone number");
+			String phone = sc.nextLine();
+			if (phoneNo.validation(phone)) {
+				System.out.println("Valid Phone number");
+			} else
+				System.out.println("Invalid Phone number");
+			System.out.println("Enter Password");
+			String passWord = sc.nextLine();
+			if (password.validation(passWord)) {
+				System.out.println("Valid Password");
+				check = true;
+			} else
+				System.out.println("Invalid Password");
 		}
 	}
 }
